e8ba196ce5dd288a2aed55cbbb89a9cb8f5c005e
28amit <amit28amical@gmail.com>
2020-04-10 00:20:37 +0530

    Update Code.c

--- Code.c
+++ Code.c
@@ -1,51 +1,110 @@
-#include<stdio.h> 
-int main() 
-{ 
-  int count,j,n,time,remain,flag=0,time_quantum; 
-  int wait_time=0,turnaround_time=0,at[10],bt[10],rt[10]; 
-  printf("Enter Total Process:\t "); 
-  scanf("%d",&n); 
-  remain=n; 
-  for(count=0;count<n;count++) 
-  { 
-    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",count+1); 
-    scanf("%d",&at[count]); 
-    scanf("%d",&bt[count]); 
-    rt[count]=bt[count]; 
-  } 
-  printf("Enter Time Quantum:\t"); 
-  scanf("%d",&time_quantum); 
-  printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n"); 
-  for(time=0,count=0;remain!=0;) 
-  { 
-    if(rt[count]<=time_quantum && rt[count]>0) 
-    { 
-      time+=rt[count]; 
-      rt[count]=0; 
-      flag=1; 
-    } 
-    else if(rt[count]>0) 
-    { 
-      rt[count]-=time_quantum; 
-      time+=time_quantum; 
-    } 
-    if(rt[count]==0 && flag==1) 
-    { 
-      remain--; 
-      printf("P[%d]\t|\t%d\t|\t%d\n",count+1,time-at[count],time-at[count]-bt[count]); 
-      wait_time+=time-at[count]-bt[count]; 
-      turnaround_time+=time-at[count]; 
-      flag=0; 
-    } 
-    if(count==n-1) 
-      count=0; 
-    else if(at[count+1]<=time) 
-      count++; 
-    else 
-      count=0; 
-  } 
-  printf("\nAverage Waiting Time= %f\n",wait_time*1.0/n); 
-  printf("Avg Turnaround Time = %f",turnaround_time*1.0/n); 
-  
-  return 0; 
+#include<stdio.h>
+struct process
+{
+    char name;
+    int AT,BT,WT,TAT,RT,CT;
+}Q1[8],Q2[16],Q3[100];/*Three queues*/
+int n;
+void sortByArrival()
+{
+struct process temp;
+int i,j;
+for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+            {
+                if(Q1[i].AT>Q1[j].AT)
+                    {
+                          temp=Q1[i];
+                          Q1[i]=Q1[j];
+                          Q1[j]=temp;
+                    }
+            }
+    }
+}
+int main()
+{
+     int i,j,k=0,r=0,time=0,tq1=8,tq2=16,flag=0;
+     char c;
+     printf("Enter no of processes:");
+     scanf("%d",&n);
+     for(i=0,c='A';i<n;i++,c++)
+     {
+         Q1[i].name=c;
+         printf("\nEnter the arrival time and burst time of process %c: ",Q1[i].name);
+         scanf("%d%d",&Q1[i].AT,&Q1[i].BT);
+         Q1[i].RT=Q1[i].BT;/*save burst time in remaining time for each process*/
+    }
+sortByArrival();
+time=Q1[0].AT;
+printf("Process in first queue following RR with qt=8");
+printf("\nProcess\t\tRT\t\tWT\t\tTAT\t\t");
+for(i=0;i<n;i++)
+{
+  if(Q1[i].RT<=tq1)
+  {
+       time+=Q1[i].RT;/*from arrival time of first process to completion of this process*/
+       Q1[i].RT=0;
+       Q1[i].WT=time-Q1[i].AT-Q1[i].BT;/*amount of time process has been waiting in the first queue*/
+       Q1[i].TAT=time-Q1[i].AT;/*amount of time to execute the process*/
+       printf("\n%c\t\t%d\t\t%d\t\t%d",Q1[i].name,Q1[i].BT,Q1[i].WT,Q1[i].TAT);
+  }
+  else/*process moves to queue 2 with qt=8*/
+  {
+      Q2[k].WT=time;
+      time+=tq1;
+      Q1[i].RT-=tq1;
+      Q2[k].BT=Q1[i].RT;
+      Q2[k].RT=Q2[k].BT;
+      Q2[k].name=Q1[i].name;
+      k=k+1;
+      flag=1;
+   }
+}
+if(flag==1)
+ {printf("\nProcess in second queue following RR with qt=16");
+  printf("\nProcess\t\tRT\t\tWT\t\tTAT\t\t");
+}for(i=0;i<k;i++)
+   {
+    if(Q2[i].RT<=tq2)
+     {
+       time+=Q2[i].RT;/*from arrival time of first process +BT of this process*/
+       Q2[i].RT=0;
+       Q2[i].WT=time-tq1-Q2[i].BT;/*amount of time process has been waiting in the ready queue*/
+       Q2[i].TAT=time-Q2[i].AT;/*amount of time to execute the process*/
+       printf("\n%c\t\t%d\t\t%d\t\t%d",Q2[i].name,Q2[i].BT,Q2[i].WT,Q2[i].TAT);
+
+    }
+    else/*process moves to queue 3 with FCFS*/
+    {
+      Q3[r].AT=time;
+      time+=tq2;
+      Q2[i].RT-=tq2;
+      Q3[r].BT=Q2[i].RT;
+      Q3[r].RT=Q3[r].BT;
+      Q3[r].name=Q2[i].name;
+      r=r+1;
+      flag=2;
+    }
+  }
+
+{if(flag==2)
+printf("\nProcess in third queue following FCFS ");
+}
+for(i=0;i<r;i++)
+{
+    if(i==0)
+            Q3[i].CT=Q3[i].BT+time-tq1-tq2;
+        else
+            Q3[i].CT=Q3[i-1].CT+Q3[i].BT;
+}
+
+for(i=0;i<r;i++)
+    {
+        Q3[i].TAT=Q3[i].CT;
+        Q3[i].WT=Q3[i].TAT-Q3[i].BT;
+        printf("\n%c\t\t%d\t\t%d\t\t%d\t\t",Q3[i].name,Q3[i].BT,Q3[i].WT,Q3[i].TAT);
+
+    }
+
 }
